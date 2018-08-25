64b0a0d5e6b1f8d6554050a3d0f5e4ce51b0372b
starcolon <tao@starcolon.com>
2018-08-25 20:37:42 +0200

    add view tournament script

--- view-tournament.js
+++ view-tournament.js
@@ -1,19 +1,22 @@
 /**
- * WC executable
+ * View tournament
  */ 
 
 var colors = require('colors');
 var F      = require('./lib/fundamental');
 
+var year = process.argv.slice(2)[0]
 
-F.loadTournament(1).then((tours) => {
+console.log('READING TOURNAMENT : ', year)
+
+F.loadTournament(year*1).then((tours) => {
 
   var tour = tours[0]
 
   // console.log(tour)
-  for (let i=0; i<2; i++){
-    console.log('GROUP : ', i)
+  for (let i=0; i<4; i++){
+    console.log('GROUP : '.cyan, i)
     console.log(tour.round[32][i])
     console.log(tour.round[32][i].fixture.map((m) => m.home + ' v ' + m.away))
   }
-})
+}).then(() => process.exit(0))
