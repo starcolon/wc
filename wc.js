e6befc14403f03cae8c0cb1206ec30c2cc8bdf2c
starcolon <tao@starcolon.com>
2018-08-25 12:12:23 +0200

    (wip) draw team into 32

--- wc.js
+++ wc.js
@@ -7,6 +7,13 @@ var F      = require('./lib/fundamental');
 
 // test init
 
-['EU','AS','AM','AF'].forEach((cont) => {
-  F.drawTeams(cont).then((teams) => console.log(teams.join(', ')));  
+// ['EU','AS','AM','AF'].forEach((cont) => {
+//   F.drawTeams(cont).then((teams) => console.log(teams.join(', ')));  
+// })
+
+
+F.newTournament().then((tour) => {
+  console.log(tour)
+  for (let i=0; i<4; i++)
+    console.log(tour.round[32][i])
 })
