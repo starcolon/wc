d39bc5a233cccf744fe015d09fc2efd5f8450220
starcolon <tao@starcolon.com>
2018-08-27 20:54:56 +0200

    render score line

--- view-tournament.js
+++ view-tournament.js
@@ -19,12 +19,17 @@ loader.do((tours) => {
   console.log('READING TOURNAMENT : ', year)
   var tour = tours[0]
 
+  var scoreToStr = function(score){
+    if (score == null) return 'v';
+    else [score.f, score.a].join('-')
+  }
+
   // console.log(tour)
   for (let i=0; i<4; i++){
     console.log('GROUP : '.cyan, i)
     console.log(tour.round[32][i])
     console.log(tour.round[32][i].fixture.map((m) => {
-      m.home + ' v ' + m.away
+      return [m.home, scoreToStr(m.score), m.away].join(' ')
     }))
   }
 }).then(() => process.exit(0))
