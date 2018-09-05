04b3395a47ef7dcee8659314bbd70fd888a422d1
starcolon <tao@starcolon.com>
2018-09-05 23:14:39 +0200

    auto create tour + proceed next match

--- wc.js
+++ wc.js
@@ -18,24 +18,67 @@ var F      = require('./lib/fundamental');
 //   process.exit(0)
 // })
 
+function padEnd(str,targetLength,padString) {
+  targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+  padString = String((typeof padString !== 'undefined' ? padString : ' '));
+  if (str.length > targetLength) {
+    return String(str);
+  }
+  else {
+    targetLength = targetLength-str.length;
+    if (targetLength > padString.length) {
+      padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+    }
+    return String(str) + padString.slice(0,targetLength);
+  }
+}
+
+function printTable(group){
+  console.log('             W  D  L '.cyan + '  F   A '.green + '  P'.magenta)
+  group.teams.forEach((t) => {
+    console.log(
+      padEnd(t.team, 12, ' '), 
+      t.w + ' ', 
+      t.d + ' ',
+      t.l + ' ',
+      (t.f < 10 ? ' ' + t.f : t.f) + ' ',
+      (t.a < 10 ? ' ' + t.a : t.a) + ' ',
+      t.pt < 10 ? ' ' + t.pt : t.pt)
+  })
+}
+
 function repl(){
   let year;
+  let tour;
+  let [nextRound, nextGroup, nextMatch] = [null,null,null]
   return F.countYears().asPromise()
     .then((yr) => {
-      year = yr;
-      return Promise.all([F.proceedTournament(yr), F.loadTournament(yr)])
+      if (yr == 0){
+        // Start a new tournament
+        year = 1;
+        return F.newTournament().then(() => Promise.all([F.proceedTournament(1), F.loadTournament(1)]))
+      }
+      else {
+        // Continue the recent tournament
+        year = yr;
+        return Promise.all([F.proceedTournament(yr), F.loadTournament(yr)])  
+      }
     })
     .then((both) => {
       let [a,tours] = both;
-      let tour = tours[0];
-      let [nextRound, nextGroup, nextMatch] = a;
+      tour = tours[0];
+      [nextRound, nextGroup, nextMatch] = a;
       let home = tour.round[nextRound][nextGroup].fixture[nextMatch].home;
       let away = tour.round[nextRound][nextGroup].fixture[nextMatch].away;
       
       console.log('YEAR  : '.green, year)
-      console.log('ROUND : '.green, nextRound)
+      console.log('ROUND : '.green, nextRound, ' #', nextGroup+1)
       console.log('GAME  : '.green, nextMatch+1)
       console.log()
+      // TAOTODO: Print out first leg if second leg
+      if (nextRound == 32){
+        printTable(tour.round[32][nextGroup])
+      }
       console.log('NEXT MATCH : '.magenta, home + ' v ' + away)
       console.log()
       return F.inputScore()
@@ -44,9 +87,20 @@ function repl(){
       let [score,scorers] = sc;
       console.log(score)
       console.log(scorers);
-      // TAOTODO:
+      return F.addFixture(year,nextRound,nextGroup,nextMatch,score,scorers)
     })
+    .then(() => {
+      if (nextRound == 32){
+        // Re-display the table after match
+        return F.loadTournament(year)
+          .do((tours) => {
+            printTable(tours[0].round[32][nextGroup])
+          })
+          .asPromise()
+      }
+      else return Promise.resolve(null)
+    })
+    // TAOTODO: Proceed the tournament
 }
 
-// TAOTODO: Repeat the REPL until the user terminates the loop
 repl()
