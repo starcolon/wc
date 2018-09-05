b620fe8310bc4595bf4f6c06f1b81c581ef50348
starcolon <tao@starcolon.com>
2018-09-05 19:24:22 +0200

    FIX view tournament to allow no argument

--- view-tournament.js
+++ view-tournament.js
@@ -8,13 +8,13 @@ var F      = require('./lib/fundamental');
 var year = process.argv.slice(2)[0]
 
 var loader = year ? 
-  F.loadTournament(year*1) : 
-  F.countYears().do((n) => {
-    year = n;
-    return F.loadTournament(n*1)
+  F.loadTournament(year*1).asPromise() : 
+  F.countYears().asPromise().then((n) => {
+    console.log(`YEAR #${n}`)
+    return F.loadTournament(n*1).asPromise()
   });
 
-loader.do((tours) => {
+loader.then((tours) => {
 
   console.log('READING TOURNAMENT : ', year)
   var tour = tours[0]
--- wc.js
+++ wc.js
@@ -7,13 +7,19 @@ var F      = require('./lib/fundamental');
 
 // F.newTournament()
 
-var scorers = [
-  '0-1 Arnautovic 10',
-  '1-1 Mahmoud 21',
-  '2-1 Mahmoud pen41',
-  '3-1 Darlem 88'
-]
-F.addFixture(1,32,0,0,'3-1',scorers).then(() => {
-  console.log('Woohooo!');
-  process.exit(0)
-})
+// var scorers = [
+//   '0-1 Arnautovic 10',
+//   '1-1 Mahmoud 21',
+//   '2-1 Mahmoud pen41',
+//   '3-1 Darlem 88'
+// ]
+// F.addFixture(1,32,0,0,'3-1',scorers).then(() => {
+//   console.log('Woohooo!');
+//   process.exit(0)
+// })
+
+function repl(){
+  
+}
+
+// TAOTODO: Repeat the REPL until the user terminates the loop
