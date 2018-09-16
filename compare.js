31ed4b7e14faa007b7ff26ee2a557e18e381fe0f
starcolon <tao@starcolon.com>
2018-09-16 21:10:14 +0200

    wip: compare teams

--- compare.js
+++ compare.js
@@ -9,6 +9,8 @@ var PriorityQueue = require('js-priority-queue');
 var team1 = process.argv.slice(2)[0];
 var team2 = process.argv.slice(2)[1];
 
+var PERF = {64: 'Did not qualify', 32: 'Group stage', 16: 'Round16', 8: 'Qtr', 4: 'Semi', 2: 'Runners-up', 1: 'CHAMPIONS'}
+
 var initAnnualPerformance = function(){
   return {round: null, w:0, d:0, l:0, f:0, a:0}
 }
@@ -16,23 +18,50 @@ var initAnnualPerformance = function(){
 var loadTeamScores = function(){
   var maxYears = 10;
   var teams = [team1, team2];
-  var results = teams.map((t) => F.loadRecentGames(maxYears,{$or: [{team: t}, {against: t}]}));
-  var scorers = teams.map((t) => F.loadRecentGoals(maxYears,{$or: [{team: t}, {against: t}]}));
 
   var annualPerformance = [initAnnualPerformance(), initAnnualPerformance()];
   var annualScorers = [{name: null, score: null}, {name: null, score: null}];
   var mostOpponentScorers = [];
 
+  var lastYear = null;
+
   F.countYears().asPromise()
-    .then((lastYear) => {
+    .then((_lastYear) => {
+      lastYear = _lastYear;
+      return Promise.all([
+          F.loadRecentGames(lastYear - maxYears,{$or: [{home: team1}, {away: team1}, {home: team2}, {away: team2}]}),
+          F.loadRecentGoals(lastYear - maxYears,{$or: [{team: team1}, {against: team1}, {team: team2}, {against: team2}]})
+        ])
+    })
+    .then(([results, scorers]) => {
+
+      console.log(`From ${results.length} matches`)
+      console.log(`From ${scorers.length} goals`)
+      console.log()
+
       for (let y=0; y<maxYears; y++){
         let Y = lastYear - y;
         if (Y<0) continue;
-        
+
         let resultsY = results.filter((a) => a.year == Y)
         let scorersY = scorers.filter((a) => a.year == Y)
 
         resultsY.sort((a,b) => a.round - b.round)
+
+        let bestPerf = [64,64];
+
+        resultsY.forEach((r) => {
+          [0,1].forEach((i) => {
+            if (r.home == teams[i] || r.away == teams[i]){
+              if (r.round < bestPerf[i]){
+                // Find the best performance so far
+                bestPerf[i] = r.round
+              }
+
+              // TAOTODO: Record win/tie/lose
+            }
+          })
+        })
 
         let topScorers = [{},{}];
         let topOppoScorers = [{},{}];
@@ -62,19 +91,21 @@ var loadTeamScores = function(){
         var topScorersSorted = [null, null];
         for (let i=0; i<2; i++){
           topScorersSorted[i] = new PriorityQueue({comparator:scoreComparer})
-          for (t in Object.entries(topScorers)){
+          for ([_,t] of Object.entries(topScorers)){
             topScorersSorted[i].queue(t)
           }
         }
 
         console.log('YEAR : ', Y)
-        console.log(topScorers[0].peek)
-        console.log(topScorers[1].peek)
+        console.log(`Best performance ${teams[0]} : ${PERF[bestPerf[0]]}`)
+        console.log(`Best performance ${teams[1]} : ${PERF[bestPerf[1]]}`)
+        console.log(topScorersSorted[0].dequeue())
+        console.log(topScorersSorted[1].dequeue())
 
         // TAOTODO:
       }
     })
+    .then(() => process.exit(0))
 }
 
-// TAOTODO:
 loadTeamScores()
