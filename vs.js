df15b53a3be58c22018bcba1a754b6fd82507a72
starcolon <tao@starcolon.com>
2019-09-11 23:10:02 +0200

    wip print game

--- vs.js
+++ vs.js
@@ -16,27 +16,12 @@ var initAnnualPerformance = function(){
   return {round: null, w:0, d:0, l:0, f:0, a:0}
 }
 
-function padEnd(str,targetLength,padString) {
-  targetLength = targetLength>>0; //floor if number or convert non-number to 0;
-  padString = String((typeof padString !== 'undefined' ? padString : ' '));
-  if (str.length > targetLength) {
-    return String(str);
-  }
-  else {
-    targetLength = targetLength-str.length;
-    if (targetLength > padString.length) {
-      padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
-    }
-    return String(str) + padString.slice(0,targetLength);
-  }
-}
-
 function loadAllMatches(team1, team2){
 
   return (lastYear) => {
     let years = lastYear - maxYears;
     return Promise.all([
-        // TAOTODO: This wont work, it causes duplicate records
+        Promise.resolve(lastYear),
         // Team 1
         F.loadRecentGames(years, {$or: [{home: team1},{away: team1}]}),
         F.loadRecentGoals(years, {$or: [{team: team1},{against: team1}]}),
@@ -52,17 +37,35 @@ function loadAllMatches(team1, team2){
   }
 }
 
+function printGame(lastYear,g,focusedTeam){
+  let yearDiff = lastYear - g.year;
+  let diff = yearDiff == 0 ? "This year" : yearDiff + " yrs ago";
+  let teamColour = (t) => {
+    if (g.outcome=='W' && focusedTeam==g.home) return t.green;
+    else if (g.outcome=='L' && focusedTeam==g.away) return t.green;
+    else if (g.outcome=='L' && focusedTeam==g.home) return t.red;
+    else if (g.outcome=='W' && focusedTeam==g.away) return t.red;
+    else return t;
+  }
+  let score = teamColour(`${g.f}-${g.a}`);
+  let home = g.home == focusedTeam ? teamColour(g.home) : g.home;
+  let away = g.away == focusedTeam ? teamColour(g.away) : g.away;
+
+  console.log(`${diff.padEnd(10)} : ${PERF[g.round]} : ${home} ${score} ${away}`)
+}
+
+function collectPastHistory(matches){
+  let [lastYear, games1, goals1, games2, goals2] = matches;
+
+  // TAOTODO:
+  for (let i=0; i<15; i++)
+    printGame(lastYear, games1[i], team1)
+}
+
 F.countYears()
   .asPromise()
   .then(loadAllMatches(team1, team2))
-  .then(([games1, goals1, games2, goals2]) => {
-
-    // console.log(`Games 1 (${team1}): ${games1.length}`)
-    // console.log(`Games 2 (${team2}): ${games2.length}`)
-    console.log(goals1.length)
-    console.log(goals2.length)
-
-  })
+  .then(collectPastHistory)
   .catch((e) => {console.log(e); process.exit(1)})
   .then(() => process.exit(0))
 
